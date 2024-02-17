d4c865dabe9aa9c38dbb51989afbf79b1a9a31a0
dietban19 <dieter.banaag@ucalgary.ca>
2024-02-17 16:54:01 -0700

    fixed nav

--- daycare-finder/src/app/layout.tsx
+++ daycare-finder/src/app/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-<<<<<<< HEAD
 import { UserProvider } from "./context/userContext";
 import React from "react";
-=======
-import Sidebar from "./_components/sidebar"
->>>>>>> 99b475366f8704eab126083f621e928c0344b707
+import Sidebar from "./_components/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,21 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-<<<<<<< HEAD
     <UserProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <div className="">
+            {/* <Sidebar /> */}
+            {children}
+          </div>
+        </body>
       </html>
     </UserProvider>
-=======
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="flex">
-          <Sidebar />
-          {children}
-        </div>
-      </body>
-    </html>
->>>>>>> 99b475366f8704eab126083f621e928c0344b707
   );
 }
