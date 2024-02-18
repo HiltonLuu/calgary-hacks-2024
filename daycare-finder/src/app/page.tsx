7fda98a0e240ce2360e36a794ce788eeb7304b03
dietban19 <dieter.banaag@ucalgary.ca>
2024-02-17 21:05:28 -0700

    feat: search bar

--- daycare-finder/src/app/page.tsx
+++ daycare-finder/src/app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
-
+import React from "react";
+import Navbar from "./_components/navbar";
+import Hero from "./_components/LandingPage/Hero";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="">
+      <Navbar />
+      <Hero />
+    </main>
   );
 }
