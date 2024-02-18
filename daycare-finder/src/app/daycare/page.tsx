3206f81e359d5fd56ded2e371302a842e61e0081
jRaimuu <liamsarj4@gmail.com>
2024-02-18 10:40:09 -0700

    Added caousel and reviews

--- daycare-finder/src/app/daycare/page.tsx
+++ daycare-finder/src/app/daycare/page.tsx
@@ -1,10 +1,60 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import React from 'react';
 import DaycareInfoCard from './daycareInfoCard';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+const StarFull = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      fillRule="evenodd"
+      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+const StarEmpty = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+    />
+  </svg>
+);
 
 function daycarePage() {
   return (
-    <div className='flex flex-col h-screen bg-purple-300'>
+    <div className='flex flex-col h-fit mb-10 bg-white'>
 
       <div className='relative h-1/2 min-h-[400px] w-full '>
         <img className='h-1/2 fixed w-full min-h-[400px] object-cover brightness-75' src='/daycarePhoto.jpg'></img>
@@ -20,7 +70,7 @@ function daycarePage() {
           <div className='flex justify-between'>
             <h1 className='text-white mb-4'>259-153 Arbour Grove Close NW, Calgary, AB</h1>
             <div className='flex flex-row gap-1'>
-              <h1 className='text-white font-medium'>4.79</h1>
+              <h1 className='text-white font-medium'>4.50</h1>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.8" stroke="currentColor" className="w-6 h-6 text-white hover:text-yellow-400">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
               </svg>
@@ -43,9 +93,22 @@ function daycarePage() {
         {/* Main Content */}
         <div className='grid grid-flow-col grid-cols-3 bg-white h-full min-h-screen w-full'>
 
+
           {/* Description */}
           <div className='flex justify-start col-span-2 '>
             <div className='mx-16 pt-10 w-full flex flex-col gap-y-4'>
+
+
+              <Carousel>
+                <CarouselContent>
+                  <CarouselItem className="basis-1/3 w-72 h-72"><img className='object-cover w-full h-full' src="https://media.kijiji.ca/api/v1/ca-prod-fsbo-ads/images/e7/e779ba9c-4702-4519-aa5c-236fae8a3b98?rule=kijijica-640-webp" alt="" /></CarouselItem>
+                  <CarouselItem className="basis-2/3 w-72 h-72"><img className='object-cover w-full h-full' src="https://media.kijiji.ca/api/v1/ca-prod-fsbo-ads/images/14/148138da-e0c0-4018-8c52-fce84999b9df?rule=kijijica-640-webp" alt="" /></CarouselItem>
+                  <CarouselItem className="basis-3/3 w-72 h-72"><img className='object-cover w-full h-full' src="https://media.kijiji.ca/api/v1/ca-prod-fsbo-ads/images/f3/f3fea19f-79c6-48de-a8df-8a01a01e286f?rule=kijijica-640-webp" alt="" /></CarouselItem>
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+              </Carousel>
+
 
               {/* About */}
               <div className='flex flex-col gap-y-4'>
@@ -117,6 +180,49 @@ function daycarePage() {
 
               <div className='bg-gray-200 my-8 h-[1px] w-full'></div>
 
+              <div className="w-full flex justify-center">
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="w-[100px]">Name</TableHead>
+                      <TableHead>Rating</TableHead>
+                      <TableHead>Review</TableHead>
+                      <TableHead className="text-right">Date Posted</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell className="font-medium">Andrew Pilot</TableCell>
+                      <TableCell className="flex">
+                        {StarFull}
+                        {StarFull}
+                        {StarFull}
+                        {StarFull}
+                        {StarFull}
+                      </TableCell>
+                      <TableCell>
+                        Ray McCoy's Daycare stands out as a beacon of excellence in Crowfoot. With Ray's attentive and nurturing care, our children not only feel safe but also thrive in their development.
+                        We couldn't be happier with our choice! 😀
+                      </TableCell>
+                      <TableCell className="text-right">10/17/2024</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell className="font-medium">Tobi Seal</TableCell>
+                      <TableCell className="flex">
+                        {StarFull}
+                        {StarFull}
+                        {StarFull}
+                        {StarFull}
+                        {StarEmpty}
+                      </TableCell>
+                      <TableCell>Ray McCoy's Daycare in Crowfoot is a game-changer for busy parents—reliable, caring, and flexible! I wish the place was closer though</TableCell>
+                      <TableCell className="text-right">2/20/2024</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </div>
+
+              <div className='bg-gray-200 my-8 h-[1px] w-full'></div>
 
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1251.9518653313357!2d-114.20747503494142!3d51.128683742101636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5371691e991451df%3A0x9f66c83462415ea0!2s153%20Arbour%20Grove%20Close%20NW%20%23259%2C%20Calgary%2C%20AB%20T3G%204J2!5e0!3m2!1sen!2sca!4v1708248788357!5m2!1sen!2sca"
@@ -128,7 +234,6 @@ function daycarePage() {
                 referrerPolicy="no-referrer-when-downgrade"
                 className='flex justify-center w-full '
               />
-
 
             </div>
           </div>
