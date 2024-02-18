43d07187a1866560758f01a01996d73167261ca5
anthonyjdam <anthonydam1968@gmail.com>
2024-02-17 23:34:25 -0700

    daycare reviews page

--- daycare-finder/src/app/auth/daycare-owner/dashboard/page.tsx
+++ daycare-finder/src/app/auth/daycare-owner/dashboard/page.tsx
@@ -1,120 +1,146 @@
-"use client"
+"use client";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 
-import { format } from "date-fns"
-import { cn } from "@/lib/utils"
-import { Calendar } from "@/components/ui/calendar"
+import { format } from "date-fns";
+import { cn } from "@/lib/utils";
+import { Calendar } from "@/components/ui/calendar";
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover"
-import { Calendar as CalendarIcon } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import { useEffect, useState } from "react"
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const childrenList = [
-    {
-        name: "Anthony",
-        time: "7:00am - 4:00pm",
-    },
-    {
-        name: "Dieter",
-        time: "7:00am - 4:00pm",
-    },
-    {
-        name: "Maira",
-        time: "7:00am - 4:00pm",
-    },
-    {
-        name: "Hilton",
-        time: "7:00am - 4:00pm",
-    },
-    {
-        name: "Liam",
-        time: "7:00am - 4:00pm",
-    },
-]
+	{
+		name: "Anthony",
+		time: "7:00am - 4:00pm",
+		image: "/assets/kyle-peyton-C0H05P-t3g4-unsplash.jpg",
+		extraInfo: "Allergic to peanuts",
+	},
+	{
+		name: "Dieter",
+		time: "7:00am - 4:00pm",
+		image: "/assets/shravan-k-acharya-cuStP_i-xPg-unsplash.jpg",
+		extraInfo: "Allergic to peanuts",
+	},
+	{
+		name: "Maira",
+		time: "7:00am - 4:00pm",
+		image: "/assets/kyle-peyton-C0H05P-t3g4-unsplash.jpg",
+		extraInfo: "Allergic to peanuts",
+	},
+	{
+		name: "Hilton",
+		time: "7:00am - 4:00pm",
+		image: "/assets/shravan-k-acharya-cuStP_i-xPg-unsplash.jpg",
+		extraInfo: "Allergic to peanuts",
+	},
+	{
+		name: "Liam",
+		time: "7:00am - 4:00pm",
+		image: "/assets/kyle-peyton-C0H05P-t3g4-unsplash.jpg",
+		extraInfo: "Allergic to peanuts",
+	},
+	{
+		name: "Liam",
+		time: "7:00am - 4:00pm",
+		image: "/assets/kyle-peyton-C0H05P-t3g4-unsplash.jpg",
+		extraInfo: "Allergic to peanuts",
+	},
+];
 
 export default function Dashboard() {
-    const [date, setDate] = useState<Date>()
+	const [date, setDate] = useState<Date>();
 
-    useEffect(() => {
-        setDate(new Date())
-    }, [])
+	useEffect(() => {
+		setDate(new Date());
+	}, []);
 
-    const month = date?.toLocaleString('en-US', { month: 'long' });
-    const day = date?.getDate();
-    const year = date?.getFullYear();
+	const month = date?.toLocaleString("en-US", { month: "long" });
+	const day = date?.getDate();
+	const year = date?.getFullYear();
 
-    const suffix = (day: any) => {
-        if (day === 1 || day === 21 || day === 31) {
-            return 'st';
-        } else if (day === 2 || day === 22) {
-            return 'nd';
-        } else if (day === 3 || day === 23) {
-            return 'rd';
-        } else {
-            return 'th';
-        }
-    };
+	const suffix = (day: any) => {
+		if (day === 1 || day === 21 || day === 31) {
+			return "st";
+		} else if (day === 2 || day === 22) {
+			return "nd";
+		} else if (day === 3 || day === 23) {
+			return "rd";
+		} else {
+			return "th";
+		}
+	};
 
-    const formattedDate = `${month} ${day}${suffix(day)}, ${year}`;
+	const formattedDate = `${month} ${day}${suffix(day)}, ${year}`;
 
+	return (
+		<div className="h-screen w-full flex flex-col items-center px-8">
+			<div className="w-full h-32 flex items-center">
+				<div className="fixed">
+					<Popover>
+						<PopoverTrigger asChild>
+							<Button
+								variant={"outline"}
+								className={cn(
+									"w-[280px] justify-start text-left font-normal",
+									!date && "text-muted-foreground"
+								)}
+							>
+								<CalendarIcon className="mr-2 h-4 w-4" />
+								{date ? format(date, "PPP") : null}
+							</Button>
+						</PopoverTrigger>
+						<PopoverContent className="w-auto p-0">
+							<Calendar
+								mode="single"
+								selected={date}
+								onSelect={setDate}
+								initialFocus
+							/>
+						</PopoverContent>
+					</Popover>
+				</div>
 
-    return (
-        <div className="h-screen w-full flex flex-col items-center p-8">
-            <div className="w-full h-32 flex gap-32">
-                <Popover>
-                    <PopoverTrigger asChild>
-                        <Button
-                            variant={"outline"}
-                            className={cn(
-                                "w-[280px] justify-start text-left font-normal",
-                                !date && "text-muted-foreground"
-                            )}
-                        >
-                            <CalendarIcon className="mr-2 h-4 w-4" />
-                            {date ? format(date, "PPP") : null}
-                        </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0">
-                        <Calendar
-                            mode="single"
-                            selected={date}
-                            onSelect={setDate}
-                            initialFocus
-                        />
-                    </PopoverContent>
-                </Popover>
-
-                <h1 className="text-xl">{date ? formattedDate : null}</h1>
-            </div>
-            <div className="flex w-full p-4 gap-4">
-                {childrenList.map((child) => {
-                    return (
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>{child.name}</CardTitle>
-                                <CardDescription>Card Description</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <p>{child.time}</p>
-                            </CardContent>
-                            <CardFooter>
-                                <p>Card Footer</p>
-                            </CardFooter>
-                        </Card>
-                    )
-                })}
-            </div>
-        </div>
-    );
+				<h1 className="text-xl text-center w-full">
+					{date ? formattedDate : null}
+				</h1>
+			</div>
+			<div className="flex flex-wrap w-full p-4 gap-4">
+				{childrenList.map((child, index) => {
+					return (
+						<Card key={index} className="w-64">
+							<CardHeader>
+								<div className="flex items-center justify-between">
+									<CardTitle>{child.name}</CardTitle>
+									<Avatar>
+										<AvatarImage className="object-cover" src={child.image} />
+										<AvatarFallback>CN</AvatarFallback>
+									</Avatar>
+								</div>
+								<CardDescription>Card Description</CardDescription>
+							</CardHeader>
+							<CardContent>
+								<p>{child.time}</p>
+							</CardContent>
+							<CardFooter>
+								<p>{child.extraInfo}</p>
+							</CardFooter>
+						</Card>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
